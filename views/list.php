cbd38be00433e5d9e9e4c491743c0be50f624436
Hiro <hiroyuki1126@gmail.com>
2021-02-11 14:14:57 +0900

    ナビゲーションバーにメニューを追加。

--- views/list.php
+++ views/list.php
@@ -31,6 +31,10 @@
         <span class="navbar-toggler-icon"></span>
       </button>
       <div id="navbarNavAltMarkup" class="collapse navbar-collapse justify-content-center">
+        <div class="navbar-nav">
+          <a class="nav-link" href=".">買い物履歴一覧</a>
+          <a class="nav-link" href=".">インポート</a>
+        </div>
       </div>
     </nav>
   </header>
@@ -116,5 +120,6 @@ foreach ($shopping_histories as $sh) {
       </form>
     </div>
   </main>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
 </body>
 </html>
