d386b5e2f98aae45addb903bad19353e88659611
Hiro <hiroyuki1126@gmail.com>
2021-02-19 21:55:03 +0900

    ナビゲーションバーを修正。

--- views/list.php
+++ views/list.php
@@ -11,16 +11,8 @@ function h($str) {
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
   <title>買い物履歴管理システム</title>
   <style>
-    @media screen and (max-width: 480px) {
-      body {
-        padding-top: 60px;
-      }
-    }
-
-    @media screen and (min-width: 1024px) {
-      body {
-        padding-top: 60px;
-      }
+    body {
+      padding-top: 4.5rem;
     }
 
     #nav {
@@ -44,16 +36,18 @@ function h($str) {
 </head>
 <body>
   <header>
-    <nav id="nav" class="navbar navbar-expand-lg fixed-top navbar-dark bg-dark">
-      <span class="navbar-brand">買い物履歴管理システム</span>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div id="navbarNavAltMarkup" class="collapse navbar-collapse justify-content-center">
-        <div class="navbar-nav">
-          <a class="nav-link" href=".">一覧表示</a>
-          <a class="nav-link" href="." data-bs-toggle="modal" data-bs-target="#import-file-modal">インポート</a>
-          <a class="nav-link" href="." data-bs-toggle="modal" data-bs-target="#help-modal">ヘルプ</a>
+    <nav id="nav" class="navbar navbar-expand-md fixed-top navbar-dark bg-dark">
+      <div class="container-fluid">
+        <span class="navbar-brand">買い物履歴管理システム</span>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup"   aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div id="navbarNavAltMarkup" class="collapse navbar-collapse">
+          <div class="navbar-nav me-auto">
+            <a class="nav-link" href=".">日毎一覧</a>
+            <span class="nav-link" data-bs-toggle="modal" data-bs-target="#import-file-modal">インポート</span>
+          </div>
+          <button class="btn btn-outline-success" type="button" data-bs-toggle="modal" data-bs-target="#help-modal">ヘルプ</button>
         </div>
       </div>
     </nav>
@@ -64,7 +58,7 @@ function h($str) {
           <div class="modal-content">
             <div class="modal-header">
               <h5 class="modal-title" id="import-file-modal-label">CSVファイルのインポート</h5>
-              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+              <button class="btn-close" type="button" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
               <p>インポートするCSVファイルを選択して下さい。</p>
