4217e835463fff3cdb1ae9493ea8a85b2ca2028b
Hiro <hiroyuki1126@gmail.com>
2021-02-22 22:01:13 +0900

    定数の初期化処理を追加。

--- controllers/create_table.php
+++ controllers/create_table.php
@@ -1,30 +1,37 @@
 <?php
+$db_host='';
+$db_name='';
+$db_user='';
+$db_pass='';
+
 require_once('/var/www/config/shopping_history/db_info.php');
 require_once(dirname(__DIR__) . '/models/categories.php');
 require_once(dirname(__DIR__) . '/models/shops.php');
 require_once(dirname(__DIR__) . '/models/orders.php');
 require_once(dirname(__DIR__) . '/models/shopping_histories.php');
 
-$dsn = "mysql:host=" . DB_HOST . ";dbname=" . DB_NAME . ";charset=utf8mb4";
-$user = DB_USER;
-$pass = DB_PASS;
+define('DB_HOST', $db_host);
+define('DB_NAME', $db_name);
+define('DB_USER', $db_user);
+define('DB_PASS', $db_pass);
 
 try {
-  $db = new PDO($dsn, $user, $pass);
+  $dsn = 'mysql:host=' . DB_HOST . ';dbname=' . DB_NAME . ';charset=utf8mb4';
+  $db = new PDO($dsn, DB_USER, DB_PASS);
   $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-  echo $e->getMessage();
+
+  $categories = new Categories($db);
+  $categories->createTable();
+
+  $shops = new Shops($db);
+  $shops->createTable();
+
+  $orders = new Orders($db);
+  $orders->createTable();
+
+  $sh = new ShoppingHistories($db);
+  $sh->createView();
+} catch (Exception $e) {
+  echo $e->getMessage(), PHP_EOL;
 }
-
-$categories = new Categories($db);
-$categories->createTable();
-
-$shops = new Shops($db);
-$shops->createTable();
-
-$orders = new Orders($db);
-$orders->createTable();
-
-$sh = new ShoppingHistories($db);
-$sh->createView();
 ?>
