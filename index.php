2e5f4f4afec2e9d58ef78b724986d4d0b78d32b6
Hiro <hiroyuki1126@gmail.com>
2021-02-28 21:40:51 +0900

    日毎一覧の処理をelse句の中に移動。

--- index.php
+++ index.php
@@ -23,67 +23,67 @@ try {
   exit(1);
 }
 
-try {
-  $sh = new ShoppingHistories($db);
-
-  $keyword = isset($_POST['input']) ? $sh->makeData($_POST['input']) : $sh->makeData(['%', '%', '%', '%', '%']);
-  foreach ($keyword as &$value) {
-    if (!$value) $value = '%';  // 検索キーワードが空であれば%に置き換える
-  }
-  $order = isset($_POST['order']) ? $_POST['order'] : '2 asc';
-
-  if (isset($_POST['operation'])) {
-    $operation = explode(',', $_POST['operation']);
-    switch ($operation[0]) {
-      case 'reset':
-        $keyword = $sh->makeData(['%', '%', '%', '%', '%']);
-        $order = '2 asc';
-        break;
-      case 'search':
-        break;
-      case 'insert':
-        $input = $sh->makeData($_POST['input']);
-        $success_message = $sh->insertData($input);
-        break;
-      case 'update':
-        $order_id = $operation[1];
-        $input = $sh->makeData($_POST['input']);
-        $success_message = $sh->updateData($order_id, $input);
-        $keyword = $sh->makeData(['%', '%', '%', '%', '%']);
-        break;
-      case 'delete':
-        $order_id = $operation[1];
-        $success_message = $sh->deleteData($order_id);
-        break;
-      case 'import':
-        $success_message = $sh->importCsv($_FILES['csv-file']);
-        break;
-    }
-  }
-} catch (Exception $e) {
-  $error_message_1 = $e->getMessage();
-}
-
-try {
-  $categories = $sh->getAllCategories();
-  $shops = $sh->getAllShops();
-  $data = $sh->getDataByKeyword($keyword, $order);
-  $number_of_data = count($data);  // データ件数
-  $sum_price = $sh->getSumPrice($keyword);  // 合計金額
-} catch (Exception $e) {
-  $error_message_2 = $e->getMessage();
-}
-
 if (isset($_GET['menu'])) {
   switch ($_GET['menu']) {
-    case 'monthly':
+    case 'monthly':  // 月毎一覧の処理
       include_once(__DIR__ . '/views/monthly_list.php');
       break;
-    case 'yearly':
+    case 'yearly':  // 年毎一覧の処理
       include_once(__DIR__ . '/views/yearly_list.php');
       break;
   }
-} else {
+} else {  // 日毎一覧の処理
+  try {
+    $sh = new ShoppingHistories($db);
+
+    $keyword = isset($_POST['input']) ? $sh->makeData($_POST['input']) : $sh->makeData(['%', '%', '%', '%', '%']);
+    foreach ($keyword as &$value) {
+      if (!$value) $value = '%';  // 検索キーワードが空であれば%に置き換える
+    }
+    $order = isset($_POST['order']) ? $_POST['order'] : '2 asc';
+
+    if (isset($_POST['operation'])) {
+      $operation = explode(',', $_POST['operation']);
+      switch ($operation[0]) {
+        case 'reset':
+          $keyword = $sh->makeData(['%', '%', '%', '%', '%']);
+          $order = '2 asc';
+          break;
+        case 'search':
+          break;
+        case 'insert':
+          $input = $sh->makeData($_POST['input']);
+          $success_message = $sh->insertData($input);
+          break;
+        case 'update':
+          $order_id = $operation[1];
+          $input = $sh->makeData($_POST['input']);
+          $success_message = $sh->updateData($order_id, $input);
+          $keyword = $sh->makeData(['%', '%', '%', '%', '%']);
+          break;
+        case 'delete':
+          $order_id = $operation[1];
+          $success_message = $sh->deleteData($order_id);
+          break;
+        case 'import':
+          $success_message = $sh->importCsv($_FILES['csv-file']);
+          break;
+      }
+    }
+  } catch (Exception $e) {
+    $error_message_1 = $e->getMessage();
+  }
+
+  try {
+    $categories = $sh->getAllCategories();
+    $shops = $sh->getAllShops();
+    $data = $sh->getDataByKeyword($keyword, $order);
+    $number_of_data = count($data);  // データ件数
+    $sum_price = $sh->getSumPrice($keyword);  // 合計金額
+  } catch (Exception $e) {
+    $error_message_2 = $e->getMessage();
+  }
+
   include_once(__DIR__ . '/views/daily_list.php');
 }
 ?>
